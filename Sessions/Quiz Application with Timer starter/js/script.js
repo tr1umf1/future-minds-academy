c0b6e7145c7c94c7c62662f354ffbe812142dca0
tr1umf1 <triumfavdyli6@gmail.com>
2024-02-08 14:37:47 +0100

    Update script.js

--- Sessions/Quiz Application with Timer starter/js/script.js
+++ Sessions/Quiz Application with Timer starter/js/script.js
@@ -3,9 +3,9 @@ const info_box = document.querySelector(".info_box");
 const exit_btn = document.querySelector(".quit");
 const start_quiz = document.querySelector(".restart");
 const quiz_box = document.querySelector(".quiz_box");
-var timer = document.querySelector(".timer_sec");
-var timer_left = document.querySelector(".time_left_txt");
-var total_que = document.querySelector(".total_que");
+const timer = document.querySelector(".timer_sec"); // Changed to const
+const timer_left = document.querySelector(".time_left_txt");
+const total_que = document.querySelector(".total_que");
 const option_list = document.querySelector(".option_list");
 const next_btn = document.querySelector(".next_btn");
 const result_box = document.querySelector(".result_box");
@@ -13,35 +13,34 @@ const restart_quiz = document.querySelector(".result_box .buttons .restart");
 const quit_quiz = document.querySelector(".result_box .buttons .quit");
 let counter = 0;
 let userScore = 0;
-
+let countdownInterval; // Variable to hold the countdown interval
 
 start_btn.addEventListener('click', (event) => {
     info_box.classList.add("activeInfo");
-})
+});
 
 exit_btn.addEventListener('click', (event) => {
     info_box.classList.remove("activeInfo");
-})
+});
 
 start_quiz.addEventListener('click', (event) => {
     info_box.classList.remove("activeInfo");
     quiz_box.classList.add("activeQuiz");
     showQuestion(counter);
     startTimer(15);
-})
+});
 
 next_btn.addEventListener('click', (event) => {
     if (counter < questions.length - 1) {
         counter++;
+        clearInterval(countdownInterval); // Clear the previous countdown interval
         startTimer(15);
         showQuestion(counter);
-        clearInterval(timer);
         resetOptions();
     } else {
         showResult();
     }
-})
-
+});
 
 function showQuestion(index) {
     let que_text = document.querySelector(".que_text");
@@ -51,7 +50,7 @@ function showQuestion(index) {
 
     let hold_options = "";
     for (let i = 0; i < questions[index].options.length; i++) {
-        hold_options += `<div class="option"><span>${questions[index].options[i]}</span></div>`;
+        hold_options += `<div class="option"><span>${questions[index].options[i]}</span><i class="fas"></i></div>`;
     }
 
     option_list.innerHTML = hold_options;
@@ -60,25 +59,26 @@ function showQuestion(index) {
     const options = document.querySelectorAll(".option");
     options.forEach(option => {
         option.addEventListener('click', () => {
-            clearInterval(timer);
+            clearInterval(countdownInterval); // Clear the countdown interval on option click
             checkAnswer(option);
         });
     });
-
 }
 
-function startTimer(count) {
-    var counter = setInterval(countDown, 1000);
-    function countDown() {
-        if (count < 10) {
-            timer.textContent = "0" + count;
-        } else { timer.textContent = count; }
 
-        if (count == 0) {
-            clearInterval(counter);
-            timer_left.innerHTML = "Time experied";
+function startTimer(count) {
+    let countValue = count; // Declare countValue to hold the countdown value
+    countdownInterval = setInterval(countDown, 1000);
+    function countDown() {
+        if (countValue < 10) {
+            timer.textContent = "0" + countValue;
+        } else { timer.textContent = countValue; }
+
+        if (countValue == 0) {
+            clearInterval(countdownInterval); // Clear the countdown interval on reaching 0
+            timer_left.innerHTML = "Time expired";
         }
-        count--;
+        countValue--;
     }
 }
 
@@ -86,19 +86,35 @@ function checkAnswer(option) {
     const userAnswer = option.querySelector("span").innerText;
     const correctAnswer = questions[counter].answer;
 
+    clearInterval(countdownInterval); // Clear the countdown interval on option click
+
     if (userAnswer === correctAnswer) {
         option.classList.add("correct");
+        option.querySelector("i").classList.add("fa-check-circle"); // Add icon for correct answer
         userScore++;
     } else {
         option.classList.add("incorrect");
+        option.querySelector("i").classList.add("fa-times-circle"); // Add icon for incorrect answer
+
+        // Highlight the correct answer
+        const options = document.querySelectorAll('.option');
+        options.forEach(opt => {
+            if (opt.querySelector("span").innerText === correctAnswer) {
+                opt.classList.add("correct");
+                opt.querySelector("i").classList.add("fa-check-circle");
+            }
+        });
     }
 
+    // Disable all options and show the next button
     const options = document.querySelectorAll('.option');
     options.forEach(option => {
         option.classList.add("disabled")
     });
     next_btn.classList.add("show");
 }
+
+
 
 function resetOptions() {
     const options = document.querySelectorAll(".options");
@@ -132,5 +148,4 @@ quit_quiz.addEventListener('click', () => {
     userScore = 0;
     result_box.classList.remove("activeResult");
     info_box.classList.add("activeInfo");
-})
-
+});
